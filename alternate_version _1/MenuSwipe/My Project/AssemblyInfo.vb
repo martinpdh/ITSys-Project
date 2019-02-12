f2e7fd90090de375b826f3a6a73a41be30d11e1b
martinpdh <42863666+martinpdh@users.noreply.github.com>
2019-02-12 15:56:44 +0100

    Add files via upload
    -Added alternate version
    -Added 5 star rating example
    -Added HTML sheet, which links all Versions and resources

---
+++ alternate_version _1/MenuSwipe/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die
+' Assemblyinformationen zu ändern.
+
+' Werte der Assemblyattribute überprüfen
+<Assembly: AssemblyTitle("MenuSwipe")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("CORDES & GRAEFE KG")>
+<Assembly: AssemblyProduct("MenuSwipe")>
+<Assembly: AssemblyCopyright("Copyright © CORDES & GRAEFE KG 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM bereitgestellt wird.
+<Assembly: Guid("8ce228c8-60a4-4459-ae4b-3040a16e93c8")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" wie folgt verwenden:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
